5b065198bcf2e6551320980e7a6c23dddf4b5dab
Kisung Nam <63442596+namks@users.noreply.github.com>
2026-05-12 11:17:22 +0900

    Refactor PRS calculation and input validation
    Updated argument handling to include common variant exclusion. Added validation for required inputs and refactored effect size mapping.

--- extdata/calculate_RareEffect_PRS.R
+++ extdata/calculate_RareEffect_PRS.R
@@ -20,8 +20,8 @@ option_list <- list(
         help="Path to group file (containing functional annotation of variants)"),
     make_option(c("--geneName"), type="character", default="",
         help="Gene name to analyze"),
-    make_option(c("--variantListFile"), type="character", default="",
-        help="Path to file containing list of variants to exclude (non-ultra-rare variants)"),
+    make_option(c("--commonVariantsToExclude"), type="character", default="",
+        help="Path to file containing list of common variant IDs to exclude from PRS calculation (one variant ID per line)"),
     make_option(c("--outputPrefix"), type="character", default="",
         help="Path to save output (without extension and gene name)")
 )
@@ -31,7 +31,17 @@ parser <- OptionParser(usage="%prog [options]", option_list=option_list)
 args <- parse_args(parser, positional_arguments = FALSE)
 print(args)
 
-## Assign varibles
+## Validate required inputs
+required_args <- c("effectFile", "bedFile", "bimFile", "famFile", "groupFile", "geneName", "outputPrefix")
+for (arg_name in required_args) {
+    if (args[[arg_name]] == "") {
+        stop(paste0("--", arg_name, " is required but not provided."))
+    }
+}
+if (!file.exists(args$effectFile)) stop(paste0("Effect file not found: ", args$effectFile))
+if (!file.exists(args$bedFile)) stop(paste0("Bed file not found: ", args$bedFile))
+
+## Assign variables
 bedFile <- args$bedFile
 bimFile <- args$bimFile
 famFile <- args$famFile
@@ -66,7 +76,11 @@ objGeno <- SAIGE::setGenoInput(bgenFile = "",
         sampleInModel = sampleID
     )
 
+## Read effect file and restore minor-allele-relative effects for flipped variants
 effect <- fread(effectFile)
+if ("is_flipped" %in% colnames(effect)) {
+    effect[is_flipped == TRUE, effect := -effect]
+}
 
 # LoF
 if (length(var_by_func_anno[[1]]) == 0) {
@@ -102,77 +116,68 @@ syn_flipped <- syn_mat_collapsed_all[[2]]
 print("Genotype matrix is loaded.")
 
 # Remove common variants
-if (args$variantListFile == "") {
-    print("No variant list file is provided.")
+if (args$commonVariantsToExclude == "") {
+    print("No common variant exclusion file is provided.")
 } else {
-    var_list <- fread(args$variantListFile)
+    var_list <- fread(args$commonVariantsToExclude, header = FALSE)
     common_var <- var_list$V1
-    lof_mat_collapsed <- lof_mat_collapsed[!which(colnames(lof_mat_collapsed) %in% common_var),]
-    mis_mat_collapsed <- mis_mat_collapsed[!which(colnames(mis_mat_collapsed) %in% common_var),]
-    syn_mat_collapsed <- syn_mat_collapsed[!which(colnames(syn_mat_collapsed) %in% common_var),]
-    print("Common variants are removed.")
+    lof_mat_collapsed <- lof_mat_collapsed[, !colnames(lof_mat_collapsed) %in% common_var, drop = FALSE]
+    mis_mat_collapsed <- mis_mat_collapsed[, !colnames(mis_mat_collapsed) %in% common_var, drop = FALSE]
+    syn_mat_collapsed <- syn_mat_collapsed[, !colnames(syn_mat_collapsed) %in% common_var, drop = FALSE]
+    print(paste0("Common variants removed. Remaining columns - lof: ", ncol(lof_mat_collapsed),
+                 ", mis: ", ncol(mis_mat_collapsed), ", syn: ", ncol(syn_mat_collapsed)))
+}
+
+## Helper: map effect sizes from effect file to genotype matrix columns
+map_effects <- function(mat, effect_df, ur_label) {
+    nc <- ncol(mat)
+    if (nc == 0) {
+        return(numeric(0))
+    }
+
+    # Get UR effect (default 0 if not in effect file)
+    ur_idx <- which(effect_df$variant == ur_label)
+    ur_effect <- if (length(ur_idx) > 0) effect_df$effect[ur_idx] else 0
+
+    # Initialize all positions with UR effect
+    eff_vec <- rep(ur_effect, nc)
+
+    # Overwrite with individual variant effects where available
+    cnames <- colnames(mat)
+    for (i in seq_len(nc)) {
+        idx <- which(effect_df$variant == cnames[i])
+        if (length(idx) > 0) {
+            eff_vec[i] <- effect_df$effect[idx]
+        }
+    }
+    return(eff_vec)
 }
 
 # Read effect size
 effect <- as.data.frame(effect)
 
-## LoF
-if (length(which(effect$variant == "lof_UR")) == 0) {
-    effect_lof_UR <- 0
-} else {
-    effect_lof_UR <- effect[which(effect$variant == "lof_UR"), ]$effect
-}
-
-effect_lof <- rep(effect_lof_UR, ncol(lof_mat_collapsed))
-
-for (i in 1:(ncol(lof_mat_collapsed) - 1)) {
-    if (length(which(effect$variant == colnames(lof_mat_collapsed)[i])) != 0) {
-        effect_lof[i] <- effect[which(effect$variant == colnames(lof_mat_collapsed)[i]), ]$effect
-    }
-}
-
-## mis
-if (length(which(effect$variant == "mis_UR")) == 0) {
-    effect_mis_UR <- 0
-} else {
-    effect_mis_UR <- effect[which(effect$variant == "mis_UR"), ]$effect
-}
-
-effect_mis <- rep(effect_mis_UR, ncol(mis_mat_collapsed))
-
-for (i in 1:(ncol(mis_mat_collapsed) - 1)) {
-    if (length(which(effect$variant == colnames(mis_mat_collapsed)[i])) != 0) {
-        effect_mis[i] <- effect[which(effect$variant == colnames(mis_mat_collapsed)[i]), ]$effect
-    }
-}
-
-## syn
-if (length(which(effect$variant == "syn_UR")) == 0) {
-    effect_syn_UR <- 0
-} else {
-    effect_syn_UR <- effect[which(effect$variant == "syn_UR"), ]$effect
-}
-
-effect_syn <- rep(effect_syn_UR, ncol(syn_mat_collapsed))
-
-for (i in 1:(ncol(syn_mat_collapsed) - 1)) {
-    if (length(which(effect$variant == colnames(syn_mat_collapsed)[i])) != 0) {
-        effect_syn[i] <- effect[which(effect$variant == colnames(syn_mat_collapsed)[i]), ]$effect
-    }
-}
+effect_lof <- map_effects(lof_mat_collapsed, effect, "lof_UR")
+effect_mis <- map_effects(mis_mat_collapsed, effect, "mis_UR")
+effect_syn <- map_effects(syn_mat_collapsed, effect, "syn_UR")
 
 print("Effect size is loaded.")
 
 ## Calculate PRS
+calc_group_prs <- function(mat, eff) {
+    if (ncol(mat) == 0) return(Matrix::Matrix(0, nrow = nrow(mat), ncol = 1, sparse = TRUE))
+    mat %*% eff
+}
 
-lof_prs <- lof_mat_collapsed %*% effect_lof
-mis_prs <- mis_mat_collapsed %*% effect_mis
-syn_prs <- syn_mat_collapsed %*% effect_syn
+lof_prs <- calc_group_prs(lof_mat_collapsed, effect_lof)
+mis_prs <- calc_group_prs(mis_mat_collapsed, effect_mis)
+syn_prs <- calc_group_prs(syn_mat_collapsed, effect_syn)
 total_prs <- lof_prs + mis_prs + syn_prs
-out <- cbind(rownames(total_prs), total_prs[,1])
-out <- as.data.frame(out)
-colnames(out) <- c("IID", "PRS")
+
+out <- data.frame(
+    IID = rownames(total_prs),
+    PRS = as.numeric(total_prs[, 1]),
+    stringsAsFactors = FALSE
+)
 
 ## Save PRS
 write.table(out, paste0(args$outputPrefix, "_", geneName, "_score.txt"), quote=FALSE, row.names=FALSE, col.names=TRUE, sep="\t")
-write.table(out, "test.txt", quote=FALSE, row.names=FALSE, col.names=TRUE, sep="\t")
